ce7475f4eae98cf05bb2c3746cfe250ce5c59b6f
dmalasky <dmalasky@byui.edu>
2021-12-08 13:21:06 -0700

    added scheam parameters

--- models/mainModel.js
+++ models/mainModel.js
@@ -5,3 +5,41 @@ const mongoose = require('mongoose');
 
 //data schema for the user
 const schema = mongoose.schema
+
+const employeeSchema = new schema({
+
+    userName: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+    
+    password: {
+        type: String,
+        required: true
+    },
+    
+    verifyPassword: {
+        type: String,
+        required: true
+    },
+
+
+    accountType: {
+        type: String,
+        required: true
+    },
+
+    phone: {
+        type: String,
+        required: true
+    },
+
+})
+
+
+//export schema for external use
