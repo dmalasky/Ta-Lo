ce69d783e9d29a8ef08a38bc10cd08423fa1289c
samuelideo <tsu17001@byui.edu>
2021-12-08 13:20:31 -0700

    function

--- controllers/mainConstroller.js
+++ controllers/mainConstroller.js
@@ -1,3 +1,11 @@
 exports.test = async (req, res, next) => {
     console.log("test")
-  };
+};
+
+exports.createUser = async (req, res, next) => {
+    console.log("Create User")
+};
+
+exports.logIn = async (req, res, next) => {
+    console.log("Login")
+};
