09a0b0c62fa430ab512cf763dbaa10f5f31d0eb5
Alex Palmer <pal19028@byui.edu>
2021-12-08 13:50:26 -0700

    Add create_user

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -5,4 +5,9 @@ const router = express.Router();
 //Routers
 router.get('/test', controller.test );
 
+//Router create_user
+router.post('/create_user', controller.createUser);
+
+//Router login
+router.post('')
 module.exports = router;
