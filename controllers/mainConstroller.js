9315e4eb12644f50e9a37fdd6b816452d721b27c
samuelideo <tsu17001@byui.edu>
2021-12-08 13:23:26 -0700

    add documentation

--- controllers/mainConstroller.js
+++ controllers/mainConstroller.js
@@ -2,10 +2,13 @@ exports.test = async (req, res, next) => {
     console.log("test")
 };
 
+//This function will create a user
 exports.createUser = async (req, res, next) => {
     console.log("Create User")
 };
 
+//Login the User
 exports.logIn = async (req, res, next) => {
     console.log("Login")
 };
+
