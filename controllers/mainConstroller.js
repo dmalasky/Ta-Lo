2efe547d96f0c61bfe115c737cc35977b517af4d
Chuqi Ding <din18003@buyi.edu>
2021-12-08 13:52:04 -0700

    documentation for the mainController.js

--- controllers/mainConstroller.js
+++ controllers/mainConstroller.js
@@ -1,9 +1,6 @@
-exports.test = async (req, res, next) => {
-    console.log("test")
-};
-
 //This function will create a user
 exports.createUser = async (req, res, next) => {
+  // User data to be saved
     const user = new User({
       userName: req.body.userName,
       email: req.body.email,
