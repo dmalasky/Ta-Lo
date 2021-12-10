0eb0abdc64331a62f1f49413a0382a7c6461779f
Alex Palmer <pal19028@byui.edu>
2021-12-10 12:50:14 -0700

    removed test route

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -2,9 +2,6 @@ const express = require('express');
 const controller = require('../controllers/mainConstroller');
 const router = express.Router();
 
-//Routers
-router.get('/test', controller.test );
-
 //Router create_user
 router.post('/createUser', controller.createUser);
 
