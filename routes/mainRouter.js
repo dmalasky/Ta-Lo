bab700d6f1ad0c8e774ab00bbd727f566e75d7e5
Candyraider7th <93948840+Candyraider7th@users.noreply.github.com>
2021-12-10 12:56:38 -0700

    remove test router (#11)

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const controller = require('../controllers/mainConstroller');
 const router = express.Router();
-
-//Routers
-router.get('/test', controller.test );
-
+ 
 //Router create_user
 router.post('/createUser', controller.createUser);
 
